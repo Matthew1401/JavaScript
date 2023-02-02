d3a90aa5b8072297be0afaaadb723c1e41b064f0
Mateusz Konieczny <mateusz.konieczny1401@gmail.com>
2023-02-02 11:13:22 +0100

    Podstawy JS

--- ZadaniaKamaKaczmarek/podstawy.js
+++ ZadaniaKamaKaczmarek/podstawy.js
@@ -1,6 +1,20 @@
 // Zadanie 1
-console.log('Hello World');
-
+console.log('Hello World')
 
 // Zadanie 2
+let hello = 'Hello World'
+console.log(hello)
+
+// Zadanie 3
+const e = 2.718228
+alert(e)
+
+// Zadanie 4
+let zmienna1 = 12
+console.log(typeof(zmienna1))
+zmienna1 = 'Język Java Script'
+console.log(typeof(zmienna1))
+
+// Zadanie 5
+
 
